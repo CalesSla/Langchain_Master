2c115be826fd60578e2363c3e82651fb5b393eea
Slava Calestru <veaceslavcalestru@gmail.com>
2023-11-01 11:27:24 +0200

    testing streaming text

---
+++ pdf/test.py
@@ -0,0 +1,21 @@
+from langchain.chat_models import ChatOpenAI
+from langchain.prompts import ChatPromptTemplate
+from langchain.chains import LLMChain
+from dotenv import load_dotenv
+load_dotenv()
+
+chat = ChatOpenAI(streaming=True)
+
+prompt = ChatPromptTemplate.from_messages([
+    ("human", "{content}")
+])
+
+chain = LLMChain(llm=chat, prompt=prompt)
+
+output = chain.stream(input = {'content': 'tell me a joke'})
+print(output)
+
+# messages = prompt.format_messages(content="tell me a joke")
+
+# for message in chat.stream(messages):
+#     print(message.content)
