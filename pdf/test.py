a4f51117eb609e721137660fce48945d96442dae
Slava Calestru <veaceslavcalestru@gmail.com>
2023-11-04 11:52:33 +0200

    pdf project finished

--- pdf/test.py
+++ pdf/test.py
@@ -1,8 +1,26 @@
 from langchain.chat_models import ChatOpenAI
 from langchain.prompts import ChatPromptTemplate
 from langchain.chains import LLMChain
+from langchain.callbacks.base import BaseCallbackHandler
+from queue import Queue
+from threading import Thread
 from dotenv import load_dotenv
 load_dotenv()
+
+
+class StreamingHandler(BaseCallbackHandler):
+    def __init__(self, queue):
+        self.queue = queue
+
+    def on_llm_new_token(self, token, **kwargs):
+        self.queue.put(token)
+
+    def on_llm_end(self, response, **kwargs):
+        self.queue.put(None)
+
+    def on_llm_error(self, error, **kwargs):
+        self.queue.put(None)
+
 
 chat = ChatOpenAI(streaming=True)
 
@@ -10,12 +28,26 @@ prompt = ChatPromptTemplate.from_messages([
     ("human", "{content}")
 ])
 
-chain = LLMChain(llm=chat, prompt=prompt)
+class StreamableChain:
+    def stream(self, input):
+        queue = Queue()
+        handler = StreamingHandler(queue)
 
-output = chain.stream(input = {'content': 'tell me a joke'})
-print(output)
+        def task():
+            self(input, callbacks=[handler ])
 
-# messages = prompt.format_messages(content="tell me a joke")
+        Thread(target=task).start()
 
-# for message in chat.stream(messages):
-#     print(message.content)
+        while True:
+            token = queue.get()
+            if token is None:
+                break
+            yield token
+
+class StreamingChain(StreamableChain, LLMChain):
+    pass
+
+chain = StreamingChain(llm=chat, prompt=prompt)
+
+for i in chain.stream(input={"content": "tell me a joke"}):
+    print(i)
